fd768952a1c4722cea4740ceb1836d72f3d9c995
P Hatami <p.hatami98@gmail.com>
2021-04-18 17:03:06 +0430

    add

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,90 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step :complete="e1 > 1" step="1">
+        parnian
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 2" step="2">
+        Name of step 2
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="3">
+        Name of step 3
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-container fluid>
+          <p>{{ radios || "null" }}</p>
+          <v-radio-group v-model="radios" mandatory>
+            <v-radio label="University" value="university"></v-radio>
+            <v-radio label="High School" value="highschool"></v-radio>
+          </v-radio-group>
+        </v-container>
+        <v-btn color="primary" @click="e1 = 2">
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-container fluid>
+          <p>{{ radios || "null" }}</p>
+          <v-radio-group
+            v-if="radios === 'university'"
+            v-model="radios2"
+            mandatory
+          >
+            <v-radio label="University" value="university"></v-radio>
+            <v-radio label="High School" value="highschool"></v-radio>
+          </v-radio-group>
+          <v-radio-group v-else v-model="radios2" mandatory>
+            <v-radio label="University" value="university"></v-radio>
+            <v-radio label="High School" value="highschool"></v-radio>
+          </v-radio-group>
+        </v-container>
+
+        <v-btn color="primary" @click="e1 = 3">
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 1">
+          Continue
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      radios: null,
+      radios2: null,
+      e1: 1
+    };
+  }
+};
+</script>
