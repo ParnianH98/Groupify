f5eedd27419d5fc8a0863bacb33f4f556d80b3bc
Delbar-Sharifzad <61940025+Delbar-Sharifzad@users.noreply.github.com>
2021-04-27 23:23:01 +0430

    added

--- utils/services.js
+++ utils/services.js
@@ -1,1 +1,28 @@
+function readyForRequest(that){
+    that.$axios.onError(async (error) => {
+        if(error.response.status === 401){
+            try{
+                res = await this.$axios.$post('api/token/refresh/', {
+                    token: localStorage.getItem('refresh')
+                })
+                localStorage.setItem('access', res.data.access_token)
+            } catch (er){
+                console.log(er)
+            }
+        }
+    })
+}
 
+export function postReq(that, url ,config){
+    if(localStorage.getItem('access')){
+        readyForRequest()
+        that.$axios.setToken(localStorage.getItem('accesss'), 'Bearer')
+        const res = that.$axios.$post(url, config)
+        return res
+    }
+    else{
+        that.$axios.setHeader('Authorization')
+        const res = that.$axios.$post(url, config)
+        return res
+    }
+}
