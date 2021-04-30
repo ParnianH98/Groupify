11d2fcf1c6bdd92f55ef1792eaf00bf86a451902
P Hatami <p.hatami98@gmail.com>
2021-04-30 04:51:45 +0430

    not complete mathching form

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,31 +2,28 @@
   <v-stepper v-model="e1">
     <v-stepper-header>
       <v-stepper-step :complete="e1 > 1" step="1">
-        Student of:
+        در کدام دوره به دنبال هم‌گروهی می‌گردید؟
       </v-stepper-step>
 
       <v-divider></v-divider>
 
-      <v-stepper-step :complete="e1 > 2" step="2">
-        Field you want to study:
+      <v-stepper-step v-if="radios === 'highschool'" step="2">
+        در چه پایه‌ای؟
+      </v-stepper-step>
+      <v-stepper-step v-if="radios === 'university'" step="2">
+        در چه مقطعی؟
       </v-stepper-step>
 
       <v-divider></v-divider>
 
-      <v-stepper-step v-if="radios === 'highschool'" step="3">
-        of grade:
+      <v-stepper-step  :complete="e1 > 3" step="3">
+        رشته تحصیلی شما چیست؟
       </v-stepper-step>
-      <v-stepper-step v-if="radios === 'university'" step="3">
-        your major is:
-      </v-stepper-step>
-      <template v-if="radios2!=='service'">
+
       <v-divider></v-divider>
 
-      <v-stepper-step v-if="radios === 'highschool'" step="4">
-        want a study-mate to study:
-      </v-stepper-step>
-      <v-stepper-step v-if="radios === 'university'" step="4">
-        want a study-mate to study:
+      <v-stepper-step  :complete="e1 > 4" step="4">
+        درس مورد نظر خود را انتخاب کنید.
       </v-stepper-step>
       </template>
 
@@ -34,13 +31,7 @@
 
 
       <v-stepper-step :complete="e1 > 5" step="5">
-        how many weeks?:
-      </v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 6" step="6">
-        how many hours per week?:
+        لطفا اطلاعات زیر را وارد کنید.
       </v-stepper-step>
 
     </v-stepper-header>
@@ -50,8 +41,8 @@
         <v-container fluid>
           <p>{{ radios || "null" }}</p>
           <v-radio-group v-model="radios" mandatory>
-            <v-radio label="University" value="university"></v-radio>
-            <v-radio label="High School" value="highschool"></v-radio>
+            <v-radio label="دانشگاه" value="university"></v-radio>
+            <v-radio label="دبیرستان" value="highschool"></v-radio>
           </v-radio-group>
         </v-container>
         <v-btn color="primary" @click="e1 = 2">
@@ -71,13 +62,13 @@
             v-model="radios2"
             mandatory
           >
-            <v-radio label="Science" value="science"></v-radio>
-            <v-radio label="Engineering" value="engineering"></v-radio>
-            <v-radio label="Service" value="service"></v-radio>
+            <v-radio label="کارشناسی" value="bsc"></v-radio>
+            <v-radio label="کارشناسی ارشد" value="msc"></v-radio>
           </v-radio-group>
           <v-radio-group v-else v-model="radios2" mandatory>
-            <v-radio label="Mathematics" value="mathematics"></v-radio>
-            <v-radio label="ٍExperimental Science" value="expscience"></v-radio>
+            <v-radio label="دهم" value="10"></v-radio>
+            <v-radio label="یازدهم" value="11"></v-radio>
+            <v-radio label="دوازدهم" value="12"></v-radio>
           </v-radio-group>
         </v-container>
 
@@ -94,37 +85,25 @@
         <v-container fluid>
           <p>{{ radios3 || "null" }}</p>
           <v-radio-group
-            v-if="radios2 === 'science'"
+            v-if="radios2 === 'bsc'"
             v-model="radios3"
             mandatory
           >
-            <v-radio label="Mathematics" value="mathematics"></v-radio>
-            <v-radio label="Computer Science" value="cs"></v-radio>
-            <v-radio label="Physics" value="physics"></v-radio>
+            <v-radio label="مهندسی کامپیوتر" value="ce"></v-radio>
+            <v-radio label="علوم کامپیوتر" value="cs"></v-radio>
+            <v-radio label="مهندسی برق" value="ee"></v-radio>
           </v-radio-group>
-          <v-radio-group v-else-if="radios2==='engineering'" v-model="radios3" mandatory>
-            <v-radio label="Computer Engineering" value="ce"></v-radio>
-            <v-radio label="Electrical Engineering" value="ee"></v-radio>
+          <v-radio-group v-else-if="radios2==='msc'" v-model="radios3" mandatory>
+            <v-radio label="هوش مصنوعی" value="َai"></v-radio>
+            <v-radio label="بیوانفورماتیک" value="bio"></v-radio>
           </v-radio-group>
-          <v-radio-group v-else-if="radios2==='service'"  v-model="radios3" mandatory>
-            <v-radio label="Calculus 1" value="calculus1"></v-radio>
-            <v-radio label="Calculus 2" value="calculus2"></v-radio>
-            <v-radio label="Physics 1" value="physics1"></v-radio>
-            <v-radio label="Physics 2" value="physics2"></v-radio>
-            <v-radio label="Others" value="otherss"></v-radio>
-          </v-radio-group>
-          <v-radio-group
-            v-else-if="radios === 'highschool'"
-            v-model="radios3"
-            mandatory
-          >
-            <v-radio label="10" value="10"></v-radio>
-            <v-radio label="11" value="11"></v-radio>
-            <v-radio label="12" value="12"></v-radio>
+          <v-radio-group v-else v-model="radios2" mandatory>
+            <v-radio label="رشته ریاضی" value="mathematics"></v-radio>
+            <v-radio label="ٍرشته تجربی" value="expscience"></v-radio>
           </v-radio-group>
           
         </v-container>
-        <v-btn color="primary" @click="ClickHandler3">
+        <v-btn color="primary" @click="e1 = 4">
           Continue
         </v-btn>
 
@@ -148,35 +127,33 @@
             <v-radio label="Machine Learning" value="ml"></v-radio>
             <v-radio label="Others" value="othersce"></v-radio>
           </v-radio-group>
-          <v-radio-group v-else-if="radios3 === 'mathematics'" v-model="radios4" mandatory>
-            <v-radio label="Probablility 1" value="probability1"></v-radio>
-            <v-radio label="Probablility 2" value="probability2"></v-radio></v-radio>
-            <v-radio label="Others" value="othersm"></v-radio>
-          </v-radio-group>
           <v-radio-group v-else-if="radios3 === 'cs'" v-model="radios4" mandatory>
             <v-radio label="Theory of Computation" value="theocomp"></v-radio>
             <v-radio label="Computer Systems" value="compsys"></v-radio>
             <v-radio label="Others" value="otherscs"></v-radio>
           </v-radio-group>
-          <v-radio-group v-else-if="radios3 === 'Physics'" v-model="radios4" mandatory>
+          <v-radio-group v-else-if="radios3 === 'ai'" v-model="radios4" mandatory>
             <v-radio label="Electromagnetic" value="electromagnetic"></v-radio>
             <v-radio label="Optic" value="optic"></v-radio>
             <v-radio label="Others" value="othersp"></v-radio>
           </v-radio-group>
           </v-radio-group>
-          <v-radio-group v-else-if="radios2==='mathematics'"  v-model="radios4" mandatory>
-            <v-radio label="Physics" value="Physics"></v-radio>
+          <v-radio-group v-else-if="radios3==='bio'"  v-model="radios4" mandatory>
+            <v-radio label="ML in Bioinformatics" value="mlinbio"></v-radio>
             <v-radio label="Mathematics" value="mathematics"></v-radio>
             <v-radio label="Geometery" value="geometery"></v-radio>
             <v-radio label="Others" value="othersm2"></v-radio>
 
           </v-radio-group>
-          <v-radio-group v-else-if="radios2==='expscience'" v-model="radios4" mandatory>
+          <v-radio-group v-else-if="radios3==='expscience'" v-model="radios4" mandatory>
             <v-radio label="Biology" value="biology"></v-radio>
             <v-radio label="Physics" value="Physics"></v-radio>
-            <v-radio label="Mathematics" value="mathematics"></v-radio>
             <v-radio label="Others" value="otherses"></v-radio>
-
+          </v-radio-group>
+          <v-radio-group v-else-if="radios3==='mathematics'" v-model="radios4" mandatory>
+            <v-radio label="Geometry" value="geometry"></v-radio>
+            <v-radio label="Calculus" value="calculus"></v-radio>
+            <v-radio label="Others" value="otherses"></v-radio>
           </v-radio-group>
         </v-container>
         <v-btn color="primary" @click="e1 = 5">
@@ -191,25 +168,57 @@
       <v-stepper-content step="5">
         <v-container fluid>
           <p>{{ radios5 || "null" }}</p>
-          
-          write what you want to do
-        </v-container>
-        <v-btn color="primary" @click="e1 = 6">
-          Continue
-        </v-btn>
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-text-field
+                    v-model="title"
+                    :rules="rules"
+                    counter="25"
+                    label="به مدت چند هفته مایلید با هم‌گروهی خود به مطالعه این درس بپردازید؟"
+                  ></v-text-field>
+                </v-col>
 
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-text-field
+                    v-model="title"
+                    :rules="wordsRules"
+                    counter="5"
+                    label="حداقل چند ساعت در هفته می‌توانید برای مطالعه این درس زمان بگذارید؟"
+                    :counter-value="v => v.trim().split(' ').length"
+                  ></v-text-field>
+                </v-col>
 
-      <v-stepper-content step="6">
-        <v-container fluid>
-          <p>{{ radios5 || "null" }}</p>
-          
-          write what you want to do
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-text-field
+                    v-model="title"
+                    :rules="wordsRules"
+                    counter="5"
+                    label="اگر توضیحات بیشتری لازم می‌دانید، ذکر کنید:"
+                    :counter-value="v => v.trim().split(' ').length"
+                  ></v-text-field>
+                </v-col>
+
+        
+              </v-row>
+            </v-container>
+          </v-form>
+
+
+
+
         </v-container>
-        <v-btn color="primary" @click="ClickHandler6">
+        <v-btn color="primary" @click="ClickHandler5">
           Continue
         </v-btn>
 
@@ -227,13 +236,7 @@
 <script>
 export default {
   methods:{
-    ClickHandler3(){
-      if (this.radios2==='service') {
-        this.$router.push({name:'dashboard'});
-      }else{
-        this.e1 = 4;
-      }
-    },ClickHandler6(){
+    ClickHandler5(){
       this.$router.push({name:'dashboard'});
     }
   },
