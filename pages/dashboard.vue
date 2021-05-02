8799e7ef17b51c632d49df6137aa4c259634e6b6
P Hatami <p.hatami98@gmail.com>
2021-05-02 19:55:39 +0430

    add

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,76 +1,94 @@
 <template>
   <div>
-    <v-row class="mt-2" align="center" justify="space-around">
-      <v-btn color="purple" @click="clickHandler('profile')">
-        پروفایل
-      </v-btn>
-      <v-btn color="light-blue accent-3" @click="clickHandler('inspire')">
-        ارسال درخواست
-      </v-btn>
-      <v-btn color="green accent-3" @click="clickHandler('requestbox')">
-        درخواست‌های دریافتی
-      </v-btn>
-    </v-row>
+    <v-row>
+      <v-col lg="2" md="2" sm="12">
+        <v-card max-width="300">
+          <v-list two-line>
+            <v-list-item-group
+              v-model="selected"
+              active-class="pink--text"
+              multiple
+            >
+              <template v-for="(item, index) in items">
+                <v-list-item :key="item.title">
+                  <template v-slot:default="{ active }">
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-text="item.title"
+                      ></v-list-item-title>
 
-    <v-card class="mt-300" max-width="500">
-      <v-list two-line>
-        <v-list-item-group
-          v-model="selected"
-          active-class="pink--text"
-          multiple
-        >
-          <template v-for="(item, index) in items">
-            <v-list-item :key="item.title">
-              <template #default="{ active }">
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.title" />
+                      <v-list-item-subtitle
+                        class="text--primary"
+                        v-text="item.headline"
+                      ></v-list-item-subtitle>
 
-                  <v-list-item-subtitle
-                    class="text--primary"
-                    v-text="item.headline"
-                  />
+                      <v-list-item-subtitle
+                        v-text="item.subtitle"
+                      ></v-list-item-subtitle>
+                    </v-list-item-content>
 
-                  <v-list-item-subtitle v-text="item.subtitle" />
-                </v-list-item-content>
+                    <v-list-item-action>
+                      <v-list-item-action-text
+                        v-text="item.action"
+                      ></v-list-item-action-text>
 
-                <v-list-item-action>
-                  <v-list-item-action-text v-text="item.action" />
+                      <v-icon v-if="!active" color="grey lighten-1">
+                        mdi-star-outline
+                      </v-icon>
 
-                  <v-icon v-if="!active" color="grey lighten-1">
-                    mdi-star-outline
-                  </v-icon>
+                      <v-icon v-else color="yellow darken-3">
+                        mdi-star
+                      </v-icon>
+                    </v-list-item-action>
+                  </template>
+                </v-list-item>
 
-                  <v-icon v-else color="yellow darken-3">
-                    mdi-star
-                  </v-icon>
-                </v-list-item-action>
+                <v-divider
+                  v-if="index < items.length - 1"
+                  :key="index"
+                ></v-divider>
               </template>
-            </v-list-item>
-
-            <v-divider v-if="index < items.length - 1" :key="index" />
-          </template>
-        </v-list-item-group>
-      </v-list>
-    </v-card>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col lg="10" md="10" sm="12">
+        <v-row class="mt-2" align="center" justify="space-around">
+          <v-btn color="purple" @click="clickHandler('profile')">
+            پروفایل
+          </v-btn>
+          <v-btn color="light-blue accent-3" @click="clickHandler('inspire')">
+            ارسال درخواست
+          </v-btn>
+          <v-btn color="green accent-3" @click="clickHandler('requestbox')">
+            درخواست‌های دریافتی
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    clickHandler(newrout) {
+      this.$router.push({ name: newrout });
+    }
+  },
   data: () => ({
     selected: [2],
     items: [
       {
         action: "15 min",
         headline: "Brunch this weekend?",
-        subtitle:
-          "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+        subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
         title: "Ali Connors"
       },
       {
         action: "2 hr",
         headline: "Summer BBQ",
-        subtitle: "Wish I could come, but I'm out of town this weekend.",
+        subtitle: `Wish I could come, but I'm out of town this weekend.`,
         title: "me, Scrott, Jennifer"
       },
       {
@@ -94,11 +112,6 @@ export default {
         title: "Britta Holt"
       }
     ]
-  }),
-  methods: {
-    clickHandler(newrout) {
-      this.$router.push({ name: newrout });
-    }
-  }
+  })
 };
 </script>
