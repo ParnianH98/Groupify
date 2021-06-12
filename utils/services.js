df29618d6c3cbadb3ce9873d809768b0b1c343bb
Delbar-Sharifzad <61940025+Delbar-Sharifzad@users.noreply.github.com>
2021-06-12 11:11:53 +0430

    api fixed

--- utils/services.js
+++ utils/services.js
@@ -1,32 +1,51 @@
-export function refreshToken(that) {
-    that.$axios.onResponseError(async error => {
-        if (localStorage.getItem("access")) {
-            if (error.response.status === 401) {
-                try {
-                    const res = await that.$axios.$post("api/token/refresh/", {
-                        token: localStorage.getItem("refresh")
-                    });
-                    that.$axios.setToken(res.data.access_token, "Bearer");
-                    return Promise.resolve(that.$axios(error.config));
-                } catch (er) {
-                    console.log(er);
-                    return Promise.reject(er);
-                }
-            } else {
-                return Promise.reject(error);
-            }
-        } else {
-            return Promise.reject(error);
-        }
-    });
+async function refreshToken(that, token) {
+    try{
+        const res = await that.$axios.$post('api/token/refresh/', {
+            refresh: token.token
+        })
+        localStorage.setItem('access', res.access)
+        localStorage.setItem('accessT', Date.now())
+    } catch (er){
+        localStorage.setItem('loggedin', false)
+        console.log(er)
+    }
 }
 
-export function postReq(that, url, config) {
+async function getReady(that) {
+    const check = localStorage.getItem('loggedin')
+    if(check){
+        const refreshTok = {token: localStorage.getItem('refresh'), time: localStorage.getItem('refreshT')}
+        console.log(refreshTok)
+        if( Date.now() - refreshTok.time >= 60*60*1000 ){
+            localStorage.setItem('loggedin', false)
+            localStorage.removeItem('access')
+            localStorage.removeItem('accessT')
+            localStorage.removeItem('refresh')
+            localStorage.removeItem('refreshT')
+        } else {
+            var accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')}
+            if ( Date.now() - accessTok.time >= 5*60*1000 ){
+                await refreshToken(that, refreshTok)
+                accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')}
+            }
+            that.$axios.setToken(accessTok.token, 'Bearer')
+        }
+    }
+}
+
+export async function postReq(that, url, config) {
+    await getReady(that)
     const res = that.$axios.$post(url, config);
     return res;
 }
-
-export function getReq(that, url, config) {
+export async function getReq(that, url, config) {
+    await getReady(that)
     const res = that.$axios.$get(url, config);
+    return res;
+}
+
+export async function delReq(that, url, config) {
+    await getReady(that)
+    const res = that.$axios.$delete(url, config);
     return res;
 }
