c5c563a088a3ffcc5fe1f3a7c770ed5d6edcbfab
P Hatami <p.hatami98@gmail.com>
2021-06-15 23:20:26 +0430

    edit

--- components/chatboard.vue
+++ components/chatboard.vue
@@ -1,124 +1,111 @@
 <template>
-    <v-container>
-        <v-responsive
-        class="overflow-y-auto"
-        max-height="300"
-        >
-            <v-list>
-                <v-list-item-title>
-                    <v-card-title>
-                        <div>
-                            {{groupeNumber}} گفتگو در گروه
-                        </div>
+  <v-container>
+    <v-responsive class="overflow-y-auto" max-height="300">
+      <v-list>
+        <v-list-item-title>
+          <v-card-title>
+            <div>{{ groupeNumber }} گفتگو در گروه</div>
 
-                        <v-btn 
-                        color="black"
-                        icon
-                        :loading="isLoadingMs" 
-                        class="mr-4" 
-                        @click="getMessages"
-                        >
-                            <v-icon> mdi-refresh </v-icon>
-                        </v-btn>
-                    </v-card-title>
-                </v-list-item-title>
-                <v-list-item v-for="(item, index) in messages" :key="index">
-                    <v-list-item-action>
-                        <v-row>
-                            <v-avatar color="indigo">
-                                {{ getAuthorInitials(item.sender) }}
-                            </v-avatar>
-                            <v-card
-                                max-width="230"
-                                min-height="24"
-                                color="indigo lighten-5"
-                            >
-                                <v-card-text> 
-                                    {{ item.text }}
-                                </v-card-text>
-                            </v-card>
-                            <v-spacer />
-                            <v-icon
-                                v-if="!item.isread"
-                                color="blue"
-                                x-small
-                                right
-                            >
-                                mdi-circle
-                            </v-icon>
-                        </v-row>
-                    </v-list-item-action>
-                </v-list-item>
-            </v-list>
-        </v-responsive>
-        <v-textarea
-            v-model="newInput"
-            append-outer-icon="mdi-send"
-            :disabled="notReadyToSend"
-            auto-grow
-            rows="1"
-            filled
-            clear-icon="mdi-close-circle"
-            clearable
-            label="New Message"
-            type="text"
-            @click:append-outer="sendMessage"
-            @click:clear="clearMessage"
-          ></v-textarea>
-    </v-container>
+            <v-btn
+              color="black"
+              icon
+              :loading="isLoadingMs"
+              class="mr-4"
+              @click="getMessages"
+            >
+              <v-icon> mdi-refresh </v-icon>
+            </v-btn>
+          </v-card-title>
+        </v-list-item-title>
+        <v-list-item v-for="(item, index) in messages" :key="index">
+          <v-list-item-action>
+            <v-row>
+              <v-avatar color="indigo">
+                {{ getAuthorInitials(item.sender) }}
+              </v-avatar>
+              <v-card max-width="230" min-height="24" color="indigo lighten-5">
+                <v-card-text>
+                  {{ item.text }}
+                </v-card-text>
+              </v-card>
+              <v-spacer />
+              <v-icon v-if="!item.isread" color="blue" x-small right>
+                mdi-circle
+              </v-icon>
+            </v-row>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-responsive>
+    <v-textarea
+      v-model="newInput"
+      append-outer-icon="mdi-send"
+      :disabled="notReadyToSend"
+      auto-grow
+      rows="1"
+      filled
+      clear-icon="mdi-close-circle"
+      clearable
+      label="New Message"
+      type="text"
+      @click:append-outer="sendMessage"
+      @click:clear="clearMessage"
+    ></v-textarea>
+  </v-container>
 </template>
 
 <script>
 import { postReq, getReq } from "~/utils/services";
 export default {
-    name: 'ChatBoard',
+  name: "ChatBoard",
 
-    props: [groupeNumber],
+  props: { groupeNumber: Number },
 
-    data() {
-        return{
-            username: '',
-            messages: [],
-            newInput: '',
-            isSendingMs: false,
-            isLoadingMs: false,
+  data() {
+    return {
+      username: "",
+      messages: [],
+      newInput: "",
+      isSendingMs: false,
+      isLoadingMs: false
+    };
+  },
+
+  computed: {
+    notReadyToSend() {
+      if (this.isSendingMs) {
+        return true;
+      } else {
+        if (this.groupeNumber === 0) {
+          return true;
         }
-    },
+      }
+    }
+  },
 
-    computed:{
-        notReadyToSend(){
-            if(this.isSendingMs) { return true }
-            else {
-                if(groupeNumber === 0){
-                    return true
-                }
-            }
-        }
+  methods: {
+    getAuthorInitials(massage) {
+      const author = massage.username;
+      const l = author.length - 1;
+      const initial = author.charAt(0) + author.charAt(l);
+      return initial;
     },
-
-    methods: {
-        getAuthorInitials(massage) {
-            const author = massage.username
-            const l = author.length - 1
-            const initial = author.charAt(0) + author.charAt(l)
-            return initial
-        },
-        clearMessage () {
-            this.newInput = ''
-        },
-        async getMessages () {
-            //get messages from api
-           this.isLoadingMs = !this.isLoadingMs
-            try{
-                const res = await getReq(this, `chat/${groupeNumber}/`)
-                console.log(res)
-                this.messages = res
-                this.isLoadingMs = !this.isLoadingMs
-            } catch (err){
-                console.log(err)
-                this.isLoadingMs = !this.isLoadingMs
-            }
-            /*this.messages = [{
+    clearMessage() {
+      this.newInput = "";
+    },
+    async getMessages() {
+      //get messages from api
+      this.isLoadingMs = !this.isLoadingMs;
+      try {
+        const res = await getReq(this, `chat/${this.groupeNumber}/`);
+        console.log(res);
+        this.messages = res;
+        this.isLoadingMs = !this.isLoadingMs;
+      } catch (err) {
+        console.log(err);
+        this.isLoadingMs = !this.isLoadingMs;
+      }
+      /*this.messages = [{
                     sender: {username: 'mohi', id:1},
                     reciver: this.groupeNumber,
                     text: 'hello',
@@ -136,30 +123,29 @@ export default {
                     text: 'سلام',
                     isread: false,
                 }]*/
-        },
-        async sendMessage () {
-            //send message to api
-            this.isSendingMs = !this.isSendingMs
-            try{
-                const res = await postReq(this, 'api/messages/',
-                {
-                    sender: this.username,
-                    reciver: groupeNumber,
-                    text: this.newInput
-                })
+    },
+    async sendMessage() {
+      //send message to api
+      this.isSendingMs = !this.isSendingMs;
+      try {
+        const res = await postReq(this, "api/messages/", {
+          sender: this.username,
+          reciver: this.groupeNumber,
+          text: this.newInput
+        });
 
-                console.log(res)
+        console.log(res);
 
-                this.getMessages()
+        this.getMessages();
 
-                this.clearMessage()
+        this.clearMessage();
 
-                this.isSendingMs = !this.isSendingMs
-            } catch(err){
-                console.log(err)
-                this.isSendingMs = !this.isSendingMs
-            }
-        },
+        this.isSendingMs = !this.isSendingMs;
+      } catch (err) {
+        console.log(err);
+        this.isSendingMs = !this.isSendingMs;
+      }
     }
-}
+  }
+};
 </script>
