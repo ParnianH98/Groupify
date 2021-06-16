bd41b1d62b803ad14668605bdb34279a7d56b75b
Delbar-Sharifzad <61940025+Delbar-Sharifzad@users.noreply.github.com>
2021-06-16 21:30:57 +0430

    fixed

--- utils/services.js
+++ utils/services.js
@@ -2,54 +2,54 @@ async function refreshToken(that, token) {
     try{
         const res = await that.$axios.$post('api/token/refresh/', {
             refresh: token.token
-        })
-        localStorage.setItem('access', res.access)
-        localStorage.setItem('accessT', Date.now())
+        });
+        localStorage.setItem('access', res.access);
+        localStorage.setItem('accessT', Date.now());
     } catch (er){
-        localStorage.setItem('loggedin', false)
-        console.log(er)
+        localStorage.setItem('loggedin', false);
+        console.log(er);
     }
 }
 
 async function getReady(that) {
-    const check = localStorage.getItem('loggedin')
+    const check = localStorage.getItem('loggedin');
     if(check === 'true'){
-        const refreshTok = {token: localStorage.getItem('refresh'), time: localStorage.getItem('refreshT')}
+        const refreshTok = {token: localStorage.getItem('refresh'), time: localStorage.getItem('refreshT')};
         if( Date.now() - refreshTok.time >= 60*60*1000 ){
-            localStorage.setItem('loggedin', false)
-            localStorage.removeItem('access')
-            localStorage.removeItem('accessT')
-            localStorage.removeItem('refresh')
-            localStorage.removeItem('refreshT')
+            localStorage.setItem('loggedin', false);
+            localStorage.removeItem('access');
+            localStorage.removeItem('accessT');
+            localStorage.removeItem('refresh');
+            localStorage.removeItem('refreshT');
         } else {
-            var accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')}
+            var accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')};
             if ( Date.now() - accessTok.time >= 5*60*1000 ){
-                await refreshToken(that, refreshTok)
-                accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')}
+                await refreshToken(that, refreshTok);
+                accessTok = {token: localStorage.getItem('access'), time: localStorage.getItem('accessT')};
             }
-            that.$axios.setToken(accessTok.token, 'Bearer')
+            that.$axios.setToken(accessTok.token, 'Bearer');
         }
     }
 }
 
 export async function postReq(that, url, config) {
-    await getReady(that)
+    await getReady(that);
     const res = that.$axios.$post(url, config);
     return res;
 }
 export async function getReq(that, url, config) {
-    await getReady(that)
+    await getReady(that);
     const res = that.$axios.$get(url, config);
     return res;
 }
 
 export async function delReq(that, url, config) {
-    await getReady(that)
+    await getReady(that);
     const res = that.$axios.$delete(url, config);
     return res;
 }
 export async function putReq(that, url, config) {
-    await getReady(that)
+    await getReady(that);
     const res = that.$axios.$put(url, config);
     return res;
 }
