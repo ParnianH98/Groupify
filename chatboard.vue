99903420574e61f8bdd2079028fb781266006295
Delbar-Sharifzad <61940025+Delbar-Sharifzad@users.noreply.github.com>
2021-06-22 20:10:46 +0430

    bug fixed

---
+++ chatboard.vue
@@ -0,0 +1,155 @@
+<template>
+  <v-container>
+    <v-card-title>
+      <div>گفتگو در گروه</div>
+
+      <v-btn
+        v-show="this.groupeNumber !== 0"
+        color="black"
+        icon
+        :loading="isLoadingMs"
+        class="mr-4"
+        @click="getMessages"
+      >
+        <v-icon> mdi-refresh </v-icon>
+      </v-btn>
+    </v-card-title>
+    <div v-if="this.groupeNumber === 0">هنوز گروهی را انتخاب نکرده اید</div>
+    <div>
+      <v-responsive v-show="this.groupeNumber !== 0" class="overflow-y-auto" max-height="300">
+        <v-list color="indigo lighten-4">
+          <v-list-item v-for="(item, index) in messages" :key="index">
+            <v-list-item-action>
+              <v-row>
+                <v-avatar :color="getColor(item)" right>
+                  {{ getAuthorInitials(item) }}
+                </v-avatar>
+                <v-spacer />
+                <v-card
+                  max-width="230"
+                  min-height="24"
+                  color="indigo lighten-5"
+                >
+                  <v-card-text>
+                    {{ item.text }}
+                  </v-card-text>
+                </v-card>
+                <v-spacer />
+                <v-icon v-if="!item.is_read" color="blue" x-small left>
+                  mdi-circle
+                </v-icon>
+              </v-row>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-responsive>
+      <v-textarea
+        v-model="newInput"
+        append-outer-icon="mdi-send"
+        :disabled="notReadyToSend"
+        auto-grow
+        rows="1"
+        filled
+        clear-icon="mdi-close-circle"
+        clearable
+        label="New Message"
+        type="text"
+        @click:append-outer="sendMessage"
+        @click:clear="clearMessage"
+      ></v-textarea>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import { postReq, putReq, getReq } from "~/utils/services";
+export default {
+  name: "ChatBoard",
+
+  props: { groupeNumber: Number, username: String},
+
+  data() {
+    return {
+      messages: [],
+      newInput: "",
+      isSendingMs: false,
+      isLoadingMs: false
+    };
+  },
+
+  watch:{
+    groupeNumber(newVal){
+      if(newVal){
+        this.getMessages();
+      }
+    }
+  },
+
+  computed: {
+    notReadyToSend() {
+      if (this.isSendingMs) {
+        return true;
+      } else {
+        if (this.groupeNumber === 0) {
+          return true;
+        }
+      }
+    }
+  },
+
+  methods: {
+    getAuthorInitials(massage) {
+      const author = massage.sender;
+      const l = author.length - 1;
+      const initial = author.charAt(0) + author.charAt(l);
+      return initial;
+    },
+    getColor(massage) {
+      const author = massage.sender;
+      if (author === this.username) {
+        return "teal lighten-1";
+      } else {
+        return "red lighten-1";
+      }
+    },
+    clearMessage() {
+      this.newInput = "";
+    },
+    async getMessages() {
+      //get messages from api
+      if(this.groupeNumber !== 0) {
+        this.isLoadingMs = !this.isLoadingMs;
+        try {
+          const res = await getReq(this, `chat/${this.groupeNumber}/`);
+          this.messages = res;
+          this.$emit("update", this.isLoadingMs);
+          await putReq(this, `chat/update/${this.groupeNumber}/`);
+          this.isLoadingMs = !this.isLoadingMs;
+        } catch (err) {
+          this.isLoadingMs = !this.isLoadingMs;
+        }
+      }
+    },
+    async sendMessage() {
+      //send message to api
+      this.isSendingMs = !this.isSendingMs;
+      try {
+        const res = await postReq(this, "api/messages/", {
+          sender: this.username,
+          receiver: this.groupeNumber,
+          is_read: false,
+          text: this.newInput
+        });
+
+        this.getMessages();
+
+        this.clearMessage();
+
+        this.isSendingMs = !this.isSendingMs;
+      } catch (err) {
+        this.isSendingMs = !this.isSendingMs;
+      }
+    }
+  }
+};
+</script>
---
+++ rategroup.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-card class="elevation-16 mx-auto" max-width="400" color="indigo lighten-4">
+    <v-card-title class="text-h5">
+      امتیازدهی
+    </v-card-title>
+    <v-card-text>
+      گروه شما پایان یافته است. به هم‌تیمی(های) خود امتیاز دهید!
+      <div class="text-right mt-12">
+        <v-col>
+          <template>
+            <v-text-field
+              :disabled="!this.isOwner"
+              label="تعداد هفته‌های فعالیت:"
+              outlined
+              dense
+              required
+              v-model="duration"
+            ></v-text-field>
+          </template>
+          <template>نام هم‌تیمی: {{ partnerN() }} </template>
+        </v-col>
+        <v-rating
+          v-model="rating"
+          color="yellow darken-3"
+          empty-icon="$ratingFull"
+          background-color="indigo lighten-5"
+          half-increments
+          hover
+          large
+        ></v-rating>
+      </div>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions class="justify-space-between">
+      <v-btn color="primary" :disabled="duration === 0" text @click="enter">
+        ثبت امتیاز
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { getReq, postReq, putReq } from "~/utils/services";
+
+export default {
+  async mounted() {
+    await this.findPartner();
+  },
+  methods: {
+    async enter() {
+      try {
+        const res = await postReq(this, `api/rating/${this.groupeNumber}/`, {
+          rate: 2 * this.rating,
+          duration: this.duration,
+          rated_user: this.partnerN(),
+          rating_user: this.username,
+          group: this.groupeNumber,
+        });
+        if (this.isOwner === false) {
+          this.rated = true;
+          this.$emit("update", this.rated);
+        }
+        if(this.isOwner === true) {
+          await putReq(this, `api/pending_update/${this.groupeNumber}/`);
+        }
+        console.log(this.isOwner);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    partnerN() {
+      var i;
+      for (i = 0; i < this.partnerName.length; i++) {
+        if (this.partnerName[i].username !== this.username) {
+          return String(this.partnerName[i].username);
+        }
+      }
+    },
+
+    async findPartner() {
+      try {
+        const res = await getReq(
+          this,
+          `api/groups/members/${this.groupeNumber}`
+        );
+        this.partnerName = res;
+        if (this.isOwner === false){
+          const resdr = await getReq(
+            this,
+            `api/rating_members/${this.groupeNumber}/`
+          );
+          this.duration = resdr[0].duration
+        }
+      } catch (err) {
+        console.log("er");
+      }
+    }
+  },
+  name: "RateGroup",
+  props: { groupeNumber: Number, username: String, isOwner: Boolean },
+
+  data: () => ({
+    rating: 0,
+    partnerName: [],
+    duration: 0,
+    rated: false
+  })
+};
+</script>
